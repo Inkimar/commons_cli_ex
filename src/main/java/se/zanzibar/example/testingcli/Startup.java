58a3b2c2ea1b5e78170754c575e49baba72e50a3
inkimar <inkimar@gmail.com>
2015-12-18 18:20:34 +0100

    Checking the file(readability) , checking the directory(readability)

--- src/main/java/se/zanzibar/example/testingcli/Startup.java
+++ src/main/java/se/zanzibar/example/testingcli/Startup.java
@@ -1,7 +1,6 @@
 package se.zanzibar.example.testingcli;
 
-import java.util.Date;
-import java.util.List;
+import java.io.File;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -22,18 +21,28 @@ public class Startup {
     private final String nameOfApp = "access2csv";
 
     private final String suffixOfApp = ".jar";
-    
-    private final String FILE ="file";
-    private final String TABLE ="table";
-    private final String COLUMN_DELIMITER ="column_delimiter";
-    private final String ROW_DELIMITER ="row_delimiter";
-    private final String QUOTE_CHARACTER ="quote-character";
-    private final String QUOTE_ESCAPE ="quote-escape";
-    private final String NO_QUOTING ="no-quoting";
-    private final String STRIP ="strip";
-    private final String LIST_ALL_TABLES ="list-all-tables";
-    private final String SCHEMA ="schema";
-    private final String PATH_TO_DIRECTORY ="directory";
+
+    private final String FILE = "file";
+
+    private final String PATH_TO_DIRECTORY = "dir";
+
+    private final String TABLE = "table";
+
+    private final String COLUMN_DELIMITER = "column_delimiter";
+
+    private final String ROW_DELIMITER = "row_delimiter";
+
+    private final String QUOTE_CHARACTER = "quote-character";
+
+    private final String QUOTE_ESCAPE = "quote-escape";
+
+    private final String NO_QUOTING = "no-quoting";
+
+    private final String STRIP = "strip";
+
+    private final String LIST_ALL_TABLES = "list-all-tables";
+
+    private final String SCHEMA = "schema";
 
     /**
      *
@@ -45,24 +54,50 @@ public class Startup {
 
         CommandLine line = start.getCommand(args);
         start.parsing(line);
-//        if (line.hasOption("file")) {
-//            String optionValue = line.getOptionValue("file");
-//            System.out.println("optionValue --file "+optionValue);
-//        }
 
     }
-    
-    private void parsing(CommandLine line){
+
+    private void parsing(CommandLine line) {
         if (line.hasOption(this.FILE)) {
-            String optionValue = line.getOptionValue(this.FILE);
-            System.out.println("optionValue for --file "+optionValue);
+            String value = line.getOptionValue(this.FILE);
+            System.out.print("\n--file " + value);
+            checkFile(value);
+        } 
+        if (line.hasOption(this.PATH_TO_DIRECTORY)) {
+            String value = line.getOptionValue(this.PATH_TO_DIRECTORY);
+            System.out.print("\n--dir " + value);
+            checkDirectory(value);
         }
+    }
+
+    private boolean checkFile(String value) {
+        boolean isReadable = false;
+        File file = new File(value);
+        if (file.exists() && file.canRead()) {
+            System.out.print(" :Able to READ the file");
+            isReadable = true;
+        } else {
+            System.out.print(" :Either the file does not exist or it is not readable");
+        }
+        return isReadable;
+    }
+
+    private boolean checkDirectory(String value) {
+        boolean isWriteable = false;
+        File file = new File(value);
+        if (file.isDirectory() && file.canWrite()) {
+            System.out.println(" :Able to write to the directory");
+            isWriteable = true;
+        } else {
+            System.out.println(" :Cannot write to the directory " + value);
+        }
+        return isWriteable;
     }
 
     private CommandLine getCommand(String[] arguments) {
         String header = "This is ".concat(nameOfApp).concat(", a command to convert MS access data to .csv. \n\n")
                 .concat("Mimimal use example:\n")
-                .concat("java -jar ").concat(nameOfApp+suffixOfApp).concat(" my.accessdb\n\n")
+                .concat("java -jar ").concat(nameOfApp + suffixOfApp).concat(" my.accessdb\n\n")
                 .concat("This will dump all tables in the my.accessdb in the current directory,\n")
                 .concat("generating one .csv file per table.\n\n")
                 .concat("Available options:");
@@ -72,32 +107,32 @@ public class Startup {
 
         try {
             Options options = new Options();
-           
-            options.addOption("f",this.FILE, true, 
+
+            options.addOption("f", this.FILE, true,
                     "the .mdb or .accdb database file"); // has key value
-            options.addOption("d",this.PATH_TO_DIRECTORY, true, 
+            options.addOption("d", this.PATH_TO_DIRECTORY, true,
                     "the path to the directory where the .csv-file(s) will be stored"); // has key value
-            options.addOption("t","table", false, 
+            options.addOption("t", "table", false,
                     "if provided only the specified table will be dumped, the default is to dump all tables except the system tables");
-            options.addOption("cd",this.COLUMN_DELIMITER, false, 
+            options.addOption("cd", this.COLUMN_DELIMITER, false,
                     "default is ,");
-            options.addOption("rd",this.ROW_DELIMITER, false, 
+            options.addOption("rd", this.ROW_DELIMITER, false,
                     "default is \\n");
-            options.addOption("qc",this.QUOTE_CHARACTER, false, 
+            options.addOption("qc", this.QUOTE_CHARACTER, false,
                     "default is \" ");
-            options.addOption("qe",this.QUOTE_ESCAPE, false, 
+            options.addOption("qe", this.QUOTE_ESCAPE, false,
                     "default is \\ - used for escaping quote characters that appears within a field");
-            options.addOption("nq",this.NO_QUOTING, false, 
+            options.addOption("nq", this.NO_QUOTING, false,
                     "activates a mode where text values are not quoted, which all text values are by default");
-            options.addOption("st",this.STRIP, false, 
+            options.addOption("st", this.STRIP, false,
                     "actives a mode where wierd characters are stripped from the output (in Access these can appear for example in MEMO fields)");
-            options.addOption("list",this.LIST_ALL_TABLES, false, 
+            options.addOption("list", this.LIST_ALL_TABLES, false,
                     "does not dump data into .csv, instead outputs a tab delimited list of all table names, excluding system tables (ie excludes any tables beginning with \"MSys\")");
-            options.addOption("s",this.SCHEMA, false, 
+            options.addOption("s", this.SCHEMA, false,
                     "outputs the DDL including indexes, relations etc in sqlite DDL sql dialect");
 
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp(nameOfApp+suffixOfApp, header, options, footer, true);
+            formatter.printHelp(nameOfApp + suffixOfApp, header, options, footer, true);
 
             CommandLineParser parser = new DefaultParser();
             cmd = parser.parse(options, arguments);
