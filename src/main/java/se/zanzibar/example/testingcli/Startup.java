bb1bc93969b5be5f1addb1d168ce6b4888f4784a
inkimar <inkimar@gmail.com>
2015-12-18 13:54:02 +0100

    refactoring, changing names

--- src/main/java/se/zanzibar/example/testingcli/Startup.java
+++ src/main/java/se/zanzibar/example/testingcli/Startup.java
@@ -10,33 +10,44 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 /**
+ * Testing the Apache Commons cli project
+ * http://commons.apache.org/proper/commons-cli/
+ *
+ * > mvn clean package > java -jar access2csv.jar -t
  *
  * @author ingimar
  */
-public class Test {
+public class Startup {
 
     /**
-     * mvn clean package java -jar Testing.jar -t
-     *
-     * http://commons.apache.org/proper/commons-cli/
-     *
-     * @param args
+     * 
+     * @param args 
      */
     public static void main(String[] args) {
 
+        Startup test = new Startup();
+        
+        CommandLine cmd = test.getCommand(args);
+        if (cmd.hasOption("t")) {
+            System.out.println("Date is " + new Date());
+        } else {
+            System.out.println("No time is given , use the '-t' option");
+        }
+    }
+
+    private CommandLine getCommand(String[] args) {
         Options options = new Options();
         options.addOption("t", false, "display current time");
+
         CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
         try {
-            CommandLine cmd = parser.parse(options, args);
-            if (cmd.hasOption("t")) {
-                System.out.println("Date is " + new Date());
-            } else {
-                System.out.println("No No");
-            }
+            cmd = parser.parse(options, args);
         } catch (ParseException ex) {
             System.err.println("Parsing failed.  Reason: " + ex.getMessage());
-            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Startup.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        return cmd;
     }
 }
